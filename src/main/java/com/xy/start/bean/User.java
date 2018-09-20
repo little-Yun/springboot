a42920bc154a5296a34509882f8b5b181b78c2e8
Xiaoy <XIAOYUN96>
2018-09-20 18:25:50 +0800

    增加spring boot读取配置文件

---
+++ src/main/java/com/xy/start/bean/User.java
@@ -0,0 +1,30 @@
+package com.xy.start.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "com.xy")
+@PropertySource("classpath:/application.properties")
+//不用这个注解，默认就是加载application.properties资源文件
+public class User {
+    private String type;
+    private String title;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
