26a0fa7659167af20b79b6d16b8acb9d9b142d51
Xiaoy <XIAOYUN96>
2019-09-20 15:29:08 +0800

    增加redis功能

---
+++ src/main/java/com/xy/study/redis/RedisTest.java
@@ -0,0 +1,35 @@
+package com.xy.study.redis;
+
+import com.xy.StudyApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = StudyApplication.class)
+public class RedisTest {
+
+    @Resource
+    private RedisUtils redisUtils;
+
+    /**
+     * 插入缓存数据
+     */
+    @Test
+    public void set() {
+        redisUtils.set("redis_key", "redis_vale");
+    }
+
+    /**
+     * 读取缓存数据
+     */
+    @Test
+    public void get() {
+        String value = redisUtils.get("redis_key");
+       log.info("value:{}",value);
+    }
+}
