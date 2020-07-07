6b32c95b5941555224d7e87e419fad5ba31bcde0
Xiaoy <XIAOYUN96>
2020-07-07 16:20:52 +0800

    代码优化

--- src/main/java/com/xy/StudyApplication.java
+++ src/main/java/com/xy/StudyApplication.java
@@ -1,15 +1,13 @@
 package com.xy;
 
-import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 
-@Slf4j
 @EnableCaching //redis缓存开启
 @SpringBootApplication
-@ComponentScan(basePackages={"com.xy"})
+@MapperScan("com.xy.dao")
 public class StudyApplication {
 
     public static void main(String[] args) {
